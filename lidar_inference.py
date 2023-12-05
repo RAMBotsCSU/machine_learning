e38875334de63a555b4a4bf3ae785dda27e9ddba
Samuel Peccoud <samuel@genofab.com>
2023-12-04 17:10:38 -0700

    Change to use runtime interpreter

--- lidar_inference.py
+++ lidar_inference.py
@@ -7,6 +7,7 @@ import cv2
 import numpy as np
 import time
 from pycoral.utils import edgetpu
+from edgetpu import edgetpu_runtime
 from pycoral.utils import dataset
 from pycoral.adapters import common
 from pycoral.adapters import classify
@@ -28,26 +29,32 @@ if __name__ == "__main__":
 
     model_path = 'lidar_model_quantized.tflite'
 
-    interpreter = load_model(model_path)
+    model_path = '/path/to/your/model_quantized.tflite'
+    interpreter = edgetpu_runtime.make_interpreter(model_path, device='usb')
+
+    # Allocate tensor memory
+    interpreter.allocate_tensors()
+
+    # interpreter = load_model(model_path)
     
+    # # input_details = interpreter.get_input_details()
     # input_details = interpreter.get_input_details()
-    input_details = interpreter.get_input_details()
-    output_details = interpreter.get_output_details()
+    # output_details = interpreter.get_output_details()
 
-    # Set input tensor data
-    fake_lidar_data = [1000] * 360
+    # # Set input tensor data
+    # fake_lidar_data = [1000] * 360
 
-    # Preprocess the input image
-    input_tensor = common.input_tensor(interpreter)
-    common.set_input(input_tensor, fake_lidar_data)
+    # # Preprocess the input image
+    # input_tensor = common.input_tensor(interpreter)
+    # common.set_input(input_tensor, fake_lidar_data)
 
-    # Run inference
-    interpreter.invoke()
+    # # Run inference
+    # interpreter.invoke()
 
-    # Get the output tensor
-    output_tensor = common.output_tensor(interpreter)
+    # # Get the output tensor
+    # output_tensor = common.output_tensor(interpreter)
 
-    print(output_tensor)
+    # print(output_tensor)
 
 
 
