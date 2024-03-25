d306b8917de4c9a261a3f03d27a9982ddb8e19e4
SuperDuper342 <lawrencemilo342@gmail.com>
2024-03-25 17:57:20 -0600

    fixed lidar model path

--- lidar_inference.py
+++ lidar_inference.py
@@ -17,8 +17,8 @@ def preprocess_lidar_data(lidar_data):
     return data_mapped
 
 if __name__ == "__main__":
-
-    model_path = 'lidar_model_quantized_edgetpu.tflite'
+    
+    model_path = 'lidar\lidar_model_quantized_edgetpu.tflite'
 
     interpreter = edgetpu.make_interpreter(model_path, device='usb')
     interpreter.allocate_tensors()
