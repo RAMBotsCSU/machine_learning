f59e32b8af3ea67456be62697a8bbf5e8a75a7e5
rkaitlin <102393649+rkaitlin@users.noreply.github.com>
2024-04-05 13:49:45 -0600

    Update tennisBallTracking.py

--- tennisBallTracking.py
+++ tennisBallTracking.py
@@ -46,7 +46,7 @@ def process_image(interpreter, image, input_index):
     # output_details[3] - count
 
     conf = (interpreter.get_tensor(output_details[0]['index'])/255)
-    positions = (interpreter.get_tensor(output_details[1]['index'])/255)
+    positions = (interpreter.get_tensor(output_details[1]['index']))
     print(conf)
     print(positions)
     print('\n')
