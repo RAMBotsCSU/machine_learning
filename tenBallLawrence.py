43ba53394f604baf62ed954c6c18f31e61e4e233
SuperDuper342 <lawrencemilo342@gmail.com>
2024-04-08 15:05:20 -0600

    fix

--- tenBallLawrence.py
+++ tenBallLawrence.py
@@ -77,18 +77,18 @@ def display_result(result, frame):
         cv2.putText(frame, 'Tennis Ball', (x1, y1), font, size, color, thickness)
         cv2.rectangle(frame, (x1, y1), (x2, y2), color, thickness)
 
+        center = bboxCenterPoint(x1)
+
+        vector_x = calculate_direction(center[0])
+
+        if vector_x > (width/2):
+           print("Turn Right!")
+        elif vector_x < (width/2):
+            print("Turn Left!")
+        else:
+            print(vector_x)
+
     cv2.imshow('Object Detection', frame)
-
-    center = bboxCenterPoint(pos * scale_x)
-
-    vector_x = calculate_direction(center[0])
-
-    if vector_x > (width/2):
-        print("Turn Right!")
-    elif vector_x < (width/2):
-        print("Turn Left!")
-    else:
-        print(vector_x)
 
 def bboxCenterPoint(position):
     bbox_center_x = int((position[0] + position[2]) / 2)
