906dd5394f3101d331d86d05d70a8ad7a4ac461e
SuperDuper342 <lawrencemilo342@gmail.com>
2024-04-12 13:42:01 -0600

    asdf

--- tenBallLawrence.py
+++ tenBallLawrence.py
@@ -45,7 +45,7 @@ def process_image(interpreter, image, input_index):
     # output_details[2] - score
     # output_details[3] - count
 
-    conf = (interpreter.get_tensor(output_details[0]['index']))
+    conf = (interpreter.get_tensor(output_details[0]['index'])/225)
     positions = (interpreter.get_tensor(output_details[1]['index']))
     print(conf, positions)
     result = []
