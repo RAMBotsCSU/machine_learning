e4fac0ab7d2ad3b5d875f3dc719f173341d8a4f6
SuperDuper342 <lawrencemilo342@gmail.com>
2024-04-12 15:16:05 -0600

    area

--- tenBall2ndDraft.py
+++ tenBall2ndDraft.py
@@ -55,6 +55,7 @@ def process_image(interpreter, image, input_index):
     for idx, score in enumerate(conf):
         pos = positions[0]
         areaPos = area(pos)
+        print(areaPos)
         if score > 0.99 and areaPos > 350 and process_image.prevAreaPos > 400:
             result.append({'pos': positions[idx]})
         process_image.prevAreaPos = areaPos  # Update prevAreaPos for the next iteration
