f085a6dbae1e987eff4692453db024aa4aa3fce5
SuperDuper342 <lawrencemilo342@gmail.com>
2024-04-17 15:34:37 -0600

    sdf

--- yourWatching.py
+++ yourWatching.py
@@ -12,7 +12,8 @@ from PIL import Image
 CAMERA_WIDTH = 640  # 640 to fill the whole screen, 320 for GUI component
 CAMERA_HEIGHT = 480  # 480 to fill the whole screen, 240 for GUI component
 INPUT_WIDTH_AND_HEIGHT = 224
-centers = 0
+centers = []
+
 
 def load_model(model_path):
     interpreter = edgetpu.make_interpreter(model_path, device='usb')
@@ -72,10 +73,10 @@ def display_result(result, frame):
 
         center = bboxCenterPoint(x1, y1, x2, y2)
         calculate_direction(center[0])
+        centers.append(center)
 
         # Draw line from object center to previous position
-        if centers is not 0:
-            cv2.line(frame, display_result.prev_center, center, color, thickness=2)
+        cv2.line(frame, centers[centers.len() - 1], center, color, thickness=2)
         centers = center  # Store current center for next frame
 
     cv2.imshow("You're Watching Disney Channel", frame)
